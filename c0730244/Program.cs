e2811de590dd4af11d183342d4ac9d32c57eef6a
visualstudio <visualstudio@230-PC26>
2019-01-24 15:00:50 -0500

    coding

--- c0730244/Program.cs
+++ c0730244/Program.cs
@@ -10,8 +10,22 @@ namespace c0730244
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(1 + 1);
+            var dog_and_cat = new car();
+        }
+    }
+    class Vehicle
+    {
+        public String Color = "Blue";
+    }
+
+    class car : Vehicle
+    {
+        public String Color = "Red";
+        public car()
+        {
+            Console.WriteLine("My color is " + Color);
             Console.ReadLine();
         }
     }
+
 }
